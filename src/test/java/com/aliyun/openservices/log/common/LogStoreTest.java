1bcabf7c0f90908b71d247515244d90898e4f827
shuizhao.gh <shuizhao.gh@alibaba-inc.com>
2026-07-02 11:59:24 +0800

    feat(logstore): add sharding policy

---
+++ src/test/java/com/aliyun/openservices/log/common/LogStoreTest.java
@@ -0,0 +1,62 @@
+package com.aliyun.openservices.log.common;
+
+import com.alibaba.fastjson.JSONObject;
+import com.aliyun.openservices.log.exception.LogException;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertTrue;
+
+public class LogStoreTest {
+
+    @Test
+    public void testShardingPolicyJsonRoundTrip() throws LogException {
+        LogStore logStore = new LogStore("test-logstore", 7, 16);
+        logStore.setShardingPolicy(new ShardingPolicy(
+                new ShardingPolicy.ShardGroup(Arrays.asList("userId"), 8),
+                new ShardingPolicy.ShardHash(Arrays.asList("instanceId", "host"), 4),
+                1764659409L));
+
+        JSONObject request = logStore.ToRequestJson();
+        assertTrue(request.containsKey("shardingPolicy"));
+        assertFalse(request.containsKey("createTime"));
+
+        JSONObject policy = request.getJSONObject("shardingPolicy");
+        assertEquals(1764659409L, policy.getLongValue("queryActiveTime"));
+        assertEquals(8, policy.getJSONObject("shardGroup").getIntValue("groupCount"));
+        assertEquals("userId", policy.getJSONObject("shardGroup").getJSONArray("keys").getString(0));
+        assertEquals(4, policy.getJSONObject("shardHash").getIntValue("maxHashCount"));
+        assertEquals("instanceId", policy.getJSONObject("shardHash").getJSONArray("keys").getString(0));
+        assertEquals("host", policy.getJSONObject("shardHash").getJSONArray("keys").getString(1));
+
+        LogStore decoded = new LogStore();
+        decoded.FromJsonObject(logStore.ToJsonObject());
+
+        assertNotNull(decoded.getShardingPolicy());
+        assertEquals(Long.valueOf(1764659409L), decoded.getShardingPolicy().getQueryActiveTime());
+        assertEquals(Arrays.asList("userId"), decoded.getShardingPolicy().getShardGroup().getKeys());
+        assertEquals(Integer.valueOf(8), decoded.getShardingPolicy().getShardGroup().getGroupCount());
+        assertEquals(Arrays.asList("instanceId", "host"), decoded.getShardingPolicy().getShardHash().getKeys());
+        assertEquals(Integer.valueOf(4), decoded.getShardingPolicy().getShardHash().getMaxHashCount());
+
+        LogStore copied = new LogStore(decoded);
+        assertNotSame(decoded.getShardingPolicy(), copied.getShardingPolicy());
+        assertEquals(decoded.ToJsonObject(), copied.ToJsonObject());
+    }
+
+    @Test
+    public void testShardHashDefaultMaxHashCount() throws LogException {
+        ShardingPolicy policy = new ShardingPolicy();
+        policy.FromJsonString("{\"shardHash\":{\"keys\":[\"instanceId\"]}}");
+
+        assertNotNull(policy.getShardHash());
+        assertEquals(Arrays.asList("instanceId"), policy.getShardHash().getKeys());
+        assertEquals(Integer.valueOf(2), policy.getShardHash().getMaxHashCount());
+        assertEquals(2, policy.ToJsonObject().getJSONObject("shardHash").getIntValue("maxHashCount"));
+    }
+}
